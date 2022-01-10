33e7e9c415fe92d9c6f2f5ab7d8f010466ebe082
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-01-10 23:21:12 +0900

    sample

---
+++ app/src/main/java/com/june/counter_mvvm_databinding/MainViewModel.kt
@@ -0,0 +1,22 @@
+package com.june.counter_mvvm_databinding
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel() : ViewModel() {
+
+    var count = MutableLiveData<Int>()
+
+    init {
+        count.value = 0
+    }
+
+    fun increase() {
+        count.value = count.value?.plus(1)
+    }
+
+    fun decrease() {
+        count.value = count.value?.minus(1)
+    }
+
+}
