f433632719572a7d0e39ebc8981b4e55a588f738
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-03-30 16:55:43 +0900

    commit before test

--- app/src/main/java/com/example/viewpager2_bottomnavigation/fragments/FragmentE.kt
+++ app/src/main/java/com/example/viewpager2_bottomnavigation/fragments/FragmentE.kt
@@ -1,21 +1,25 @@
 package com.example.viewpager2_bottomnavigation.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.viewpager2_bottomnavigation.activitiy.MainActivity
 import com.example.viewpager2_bottomnavigation.activitiy.MainActivity.Companion.TAG
 import com.example.viewpager2_bottomnavigation.adapter.RecyclerAdapterE
 import com.example.viewpager2_bottomnavigation.adapter.RecyclerAdapterE.Companion.itemList
-import com.example.viewpager2_bottomnavigation.databinding.FragmentCBinding
 import com.example.viewpager2_bottomnavigation.databinding.FragmentEBinding
+import com.example.viewpager2_bottomnavigation.viewmodel.ViewModelE
 
 class FragmentE : Fragment() {
     private var binding: FragmentEBinding?= null
+    private val viewModel: ViewModelE by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val fragmentBinding = FragmentEBinding.inflate(inflater, container, false)
@@ -27,8 +31,20 @@ class FragmentE : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         Log.d(TAG, ">>>>>onCreateView: FragmentE")
 
+        binding?.lifecycleOwner = viewLifecycleOwner
+        binding?.viewModel = viewModel
+
         initRecyclerView()
-        initButton()
+//        initButton()
+
+        viewModel.currentItemListSize.observe(viewLifecycleOwner, Observer {
+
+
+
+
+
+            binding?.recyclerView?.adapter?.notifyDataSetChanged()
+        })
     }
 
     override fun onDestroy() {
@@ -44,18 +60,19 @@ class FragmentE : Fragment() {
         binding?.recyclerView?.layoutManager = layoutManager
     }
 
-    private fun initButton() {
-        binding?.plusButton?.setOnClickListener {
-            val _number = itemList.size + 1
-            val number = _number.toString()
-            itemList.add(number)
-            binding?.recyclerView?.adapter?.notifyDataSetChanged()
-        }
-        binding?.minusButton?.setOnClickListener {
-            if (itemList.isNotEmpty()) {
-                itemList.removeAt(itemList.size-1)
-                binding?.recyclerView?.adapter?.notifyDataSetChanged()
-            }
-        }
-    }
+//MVC
+//    private fun initButton() {
+//        binding?.plusButton?.setOnClickListener {
+//            val _number = itemList.size + 1
+//            val number = _number.toString()
+//            itemList.add(number)
+//            binding?.recyclerView?.adapter?.notifyDataSetChanged()
+//        }
+//        binding?.minusButton?.setOnClickListener {
+//            if (itemList.isNotEmpty()) {
+//                itemList.removeAt(itemList.size-1)
+//                binding?.recyclerView?.adapter?.notifyDataSetChanged()
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/example/viewpager2_bottomnavigation/viewmodel/ViewModelE.kt
+++ app/src/main/java/com/example/viewpager2_bottomnavigation/viewmodel/ViewModelE.kt
@@ -1,6 +1,34 @@
 package com.example.viewpager2_bottomnavigation.viewmodel
 
-class ViewModelE {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.viewpager2_bottomnavigation.activitiy.MainActivity.Companion.TAG
+import com.example.viewpager2_bottomnavigation.adapter.RecyclerAdapterE.Companion.itemList
 
+class ViewModelE: ViewModel() {
+    private val _currentItemListSize = MutableLiveData<Int>()
+    //private val _currentItemListSize = MutableLiveData<MutableList<String>>()
 
+    val currentItemListSize: LiveData<Int>
+        get() = _currentItemListSize
+
+    init {
+        _currentItemListSize.value = itemList.size
+    }
+
+    fun addItem() {
+        val _number = itemList.size + 1
+        val number = _number.toString()
+        itemList.add(number)
+        _currentItemListSize.value = itemList.size + 1
+    }
+
+    fun subItem() {
+        if (itemList.isNotEmpty()) {
+            itemList.removeAt(itemList.size-1)
+            _currentItemListSize.value = itemList.size + 1
+        }
+    }
 }
