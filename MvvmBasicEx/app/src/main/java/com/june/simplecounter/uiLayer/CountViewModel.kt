f87a6d913c0ee0cf845b55b44b87a20d6b0b6a8e
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-12-23 17:11:50 +0900

    MvvmBasicEx: stateHandle CountViewModel

--- MvvmBasicEx/app/src/main/java/com/june/simplecounter/uiLayer/CountViewModel.kt
+++ MvvmBasicEx/app/src/main/java/com/june/simplecounter/uiLayer/CountViewModel.kt
@@ -2,18 +2,23 @@ package com.june.simplecounter.uiLayer
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
 import com.june.simplecounter.dataLayer.CountLocalDataSource
 import com.june.simplecounter.dataLayer.CountLocalDataSource.Companion.INIT_VALUE_ALIAS
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.june.simplecounter.uiLayer.SavableMutableStateFlow.Companion.KEY_COUNT
 import kotlinx.coroutines.flow.StateFlow
 
-class CountViewModel(application: Application) : AndroidViewModel(application) {
-    private val context = getApplication<Application>().applicationContext
-    private val countLocalDataSource = CountLocalDataSource.getInstance(context)
+class CountViewModel(
+    application: Application,
+    stateHandle: SavedStateHandle
+) : AndroidViewModel(application) {
+    private val countLocalDataSource = CountLocalDataSource.getInstance(
+        _context = getApplication<Application>().applicationContext
+    )
 
-    private var _count =  MutableStateFlow(0)
+    private val _count = stateHandle.getMutableStateFlow(KEY_COUNT, 0)
     val count: StateFlow<Int>
-        get() = _count
+        get() = _count.asStateFlow()
 
     init {
         _count.value = countLocalDataSource.getInt(INIT_VALUE_ALIAS, 0)
