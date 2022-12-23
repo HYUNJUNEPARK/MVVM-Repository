71a32e4e4ab386f8a3ae0333a1141b451154f6a2
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-12-23 15:48:54 +0900

    commit

--- SimpleCounter/app/src/main/java/com/june/simplecounter/network/model/response/License.kt
+++
@@ -1,9 +0,0 @@
-package com.june.simplecounter.network.model.response
-
-data class License(
-    val key: String,
-    val name: String,
-    val node_id: String,
-    val spdx_id: String,
-    val url: String
-)
--- SimpleCounter/app/src/main/java/com/june/simplecounter/network/model/response/Repository.kt
+++
@@ -1,3 +0,0 @@
-package com.june.simplecounter.network.model.response
-
-class Repository : ArrayList<RepositoryItem>()
